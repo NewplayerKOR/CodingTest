ec713fc185616b181d1d673fa2fe66d1e25bb07d
NewplayerKOR <149351553+NewplayerKOR@users.noreply.github.com>
2026-04-08 01:03:08 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,10 +1,24 @@
 class Solution {
+    // 유클리드 호제법
+    // GCD(greatest common divisor) * LCM(least common multiple) = A * B
+    // GCD = (A * B) / LCM
+    // LCM = (A * B) / GCD
+
     public int solution(int n) {
         int answer = 1;
-        while ((answer * 6) % n != 0) {
-            answer++;
-        }
+
+        int lcm = (n * 6) / gcd(n, 6);
+        answer = lcm / 6;
 
         return answer;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
     }
 }
